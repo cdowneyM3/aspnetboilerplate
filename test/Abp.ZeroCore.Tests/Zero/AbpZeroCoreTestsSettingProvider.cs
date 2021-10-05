8f09ab540867cafe51f48580a23b5664b5082303
Musa Demir <demirmusa96@gmail.com>
2021-10-05 23:34:02 +0300

    added use of PerRequestRedisCache to SettingManager
    Use Configuration.UsePerRequestRedisCacheForSettingManager(); in PreInitialize to use PerRequestRedisCache in setting manager

---
+++ test/Abp.ZeroCore.Tests/Zero/AbpZeroCoreTestsSettingProvider.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Abp.Authorization;
+using Abp.Configuration;
+
+namespace Abp.Zero
+{
+    public class AbpZeroCoreTestsSettingProvider : SettingProvider
+    {
+        public const string TestSetting1 = "AbpWebCommonTestModule.Test.Setting1";
+        public const string TestSetting2 = "AbpWebCommonTestModule.Test.Setting2";
+
+        public override IEnumerable<SettingDefinition> GetSettingDefinitions(SettingDefinitionProviderContext context)
+        {
+            return new[]
+            {
+                new SettingDefinition(
+                    TestSetting1,
+                    "TestValue1",
+                    scopes: SettingScopes.Application | SettingScopes.Tenant | SettingScopes.User,
+                    clientVisibilityProvider: new VisibleSettingClientVisibilityProvider()),
+
+                new SettingDefinition(
+                    TestSetting2,
+                    "TestValue2",
+                    scopes: SettingScopes.Application | SettingScopes.Tenant | SettingScopes.User,
+                    clientVisibilityProvider: new RequiresPermissionSettingClientVisibilityProvider(
+                        new SimplePermissionDependency("Permission1")))
+            };
+        }
+    }
+}
